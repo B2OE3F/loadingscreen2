841d2adf35800597b47db7729a6235121e30d2c4
B2OE3F <dylandiericx0201@gmail.com>
2024-11-03 23:31:45 +0100

    Add files via upload

---
+++ Loadingscreen/html/assets/js/config.js
@@ -0,0 +1,29 @@
+const Config = {
+    links: {
+        discord: "discord.gg/L.A City",
+        tebex: "L.A SHOP.tebex.io/",
+    },
+    settings: {
+        volume: 0.7,
+    },
+    audios: [
+        {
+            title: "NLE - Shotta Flow",
+            file: "music.mp3",
+        },
+        {
+            title: "pop smoke - Dior",
+            file: "music2.mp3",
+        },
+    ],
+    staff: [
+        {
+            role: "Staff",
+            name: "Daoud_oslm"
+        },
+        {
+            role: "Modérateur",
+            name: "alrez"
+        },
+    ]
+}
